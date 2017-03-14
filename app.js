7c8535e0e484be0bbf50695ef9e856089fe36892
Andrei Zoitanu <zoitanu.andrei@yahoo.com>
2017-03-14 21:32:18 +0100

    fdwsfd

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var cors = require('cors');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -11,6 +12,7 @@ var LocalStrategy = require('passport-local').Strategy;
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
 
+app.use(cors({origin: 'http://89.46.104.211'}));
 
 
 mongoose.connect('mongodb://104.236.43.92/loginapp');
