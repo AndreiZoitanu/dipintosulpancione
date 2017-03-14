c257f76e3c9c669fe003dec6f892d9eb6bcd1040
Andrei Zoitanu <zoitanu.andrei@yahoo.com>
2017-03-14 08:37:34 +0100

    mongodb production

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var mongoose = require('mongoose');
 
 
 
-mongoose.connect('mongodb://localhost/loginapp');
+mongoose.connect('mongodb://104.236.43.92/loginapp');
 var db = mongoose.connection;
 
 var routes = require('./routes/index');
@@ -84,7 +84,7 @@ app.use('/users', users);
 app.use('/administration', administration);
 
 // Set Port
-app.set('port', (process.env.PORT || 3000));
+app.set('port', (process.env.PORT || 8080));
 
 app.listen(app.get('port'), function(){
 	console.log('Server started on port '+app.get('port'));
