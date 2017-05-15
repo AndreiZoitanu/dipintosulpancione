28f0624daa4432c67bb29448aad76c8f02defffa
Andrei Zoitanu <zoitanu.andrei@yahoo.com>
2017-05-16 00:17:44 +0200

    dsafds

--- routes/documenti.js
+++ routes/documenti.js
@@ -4,10 +4,11 @@ var multiparty = require('multiparty');
 var fs = require('fs')
 var s3 = require('s3');
 var AWS = require('aws-sdk');
+var open = require('open');
 
 AWS.config.update({
-    accessKeyId: "AKIAI5L3Q6XPQFC4L6BQ",
-    secretAccessKey: "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",
+    accessKeyId: "AKIAI756VY4RDEPZ75KQ",
+    secretAccessKey: "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",
     signatureVersion: 'v4'
   });
 
@@ -17,23 +18,49 @@ var options = {
   s3Client: awsS3Client,
 };
 var client = s3.createClient(options);
-
+var bucket = 'gestionecasa';
 
 router.get('/bollette-pagate', ensureAuthenticated, function(req, res){
   awsS3Client.listObjects({Bucket: 'gestionecasa'}, function(err,data) {
     if (err)
       console.log(err);
     else{
-      data.Contents.forEach(function(element){
-        console.log(element.Key);
+      var files = [];
+      var folder = '';
+      data.Contents.forEach(function(element, index){
+        if (index == 0){
+          folder = element.Key;
+        }else{
+          if(element.Key.startsWith(folder)){
+            files.push(element.Key);
+          }
+        }
       });
-    res.render('documenti/bollette-pagate');
+
+    res.render('documenti/bollette-pagate', files);
     }
   })
 });
 
 router.get('/bollette-da-pagare', ensureAuthenticated, function(req, res){
-  res.render('documenti/bollette-da-pagare');
+  awsS3Client.listObjects({Bucket: 'gestionecasa'}, function(err,data) {
+    if (err)
+      console.log(err);
+    else{
+      var files = [];
+      var folder = '';
+      data.Contents.forEach(function(element, index){
+        if (index == 0){
+          folder = element.Key;
+        }else{
+          if(element.Key.startsWith(folder)){
+            files.push(element.Key.replace('/', '@'));
+          }
+        }
+      });
+    res.render('documenti/bollette-da-pagare', {'files': files});
+    }
+  })
 });
 
 router.get('/fotocopie', ensureAuthenticated, function(req, res){
@@ -43,6 +70,48 @@ router.get('/fotocopie', ensureAuthenticated, function(req, res){
 router.get('/altro', ensureAuthenticated, function(req, res){
   res.render('documenti/altro');
 });
+
+
+router.get('/get_aws_file/:file', ensureAuthenticated, function(req, res){
+
+
+  var x = awsS3Client.getSignedUrl('getObject',{Bucket: bucket, Key:req.params.file.replace('@', '/')})
+  open( x, function (err) {
+    if ( err ) throw err;
+  });
+  res.redirect('back');
+});
+
+
+router.post('/aws_upload', ensureAuthenticated, function(req, res){
+  var form = new multiparty.Form();
+  form.parse(req, function(err, fields, files) {
+
+    var file_extension = files.file[0].originalFilename.split('.')[1];
+
+    var params = {
+     localFile: files.file[0].path,
+     s3Params: {
+       Bucket: bucket,
+       Key: fields.folder + '/' + fields.name + '.' + file_extension
+     },
+    };
+
+    var uploader = client.uploadFile(params);
+    uploader.on('error', function(err) {
+      console.error("unable to upload:", err.stack);
+    });
+    uploader.on('progress', function() {
+      console.log("progress", uploader.progressMd5Amount,
+                uploader.progressAmount, uploader.progressTotal);
+    });
+    uploader.on('end', function() {
+      res.redirect('back');
+    });
+  });
+});
+
+
 
 
 function getFiles(bucket){
